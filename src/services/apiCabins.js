5d46ec43498da9870a5a85a4c6097b40b9d1f4aa
Adam Rao <adamokoth@protonmail.com>
2024-08-16 01:18:29 +0300

    feat: creatate new cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -5,22 +5,33 @@ export default async function getCabins() {
 
   if (error) {
     console.error(error);
-    throw new Error("Cabins could not be loaded");
+    throw new Error("Cabin could not be loaded");
+  }
+
+  return data;
+}
+
+export async function createCabin(cabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([cabin])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be created");
   }
 
   return data;
 }
 
 export async function deleteCabin(id) {
-  const { data, error } = await supabase
-    .from('cabins')
-    .delete()
-    .eq('id', id);
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
 
-    if (error) {
-      console.error(error);
-      throw new Error("Cabins could not be deleted");
-    }
-  
-    return data;
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be deleted");
+  }
+
+  return data;
 }

a6491d95bfff363fece71fdca613b6bd69bce617
Adam Rao <adamokoth@protonmail.com>
2024-09-09 00:45:07 +0300

    refactor: modal to a compound component

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,21 +1,24 @@
-import { useState } from "react";
-
 import Button from "../../ui/Button";
+import CabinTable from "./CabinTable";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
 
 export default function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
   return (
-    <>
-      <Button onClick={() => setIsOpenModal(!isOpenModal)}>
-        Add new cabin
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)}/>
-        </Modal>
-      )}
-    </>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add New Cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opens="table">
+        <Button>Show table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
   );
 }

820f9b7f6964a2caf38c7a018f8cb7fbb6eaaf95
Adam Rao <adamokoth@protonmail.com>
2024-09-10 07:19:19 +0300

    chore: create useOutsideClick custom hook:

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,7 +1,14 @@
-import { cloneElement, createContext, useContext, useState } from "react";
+import {
+  cloneElement,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
+import useOutsideClick from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -75,12 +82,25 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
   const { close, openName } = useContext(ModalContext);
+  const ref = useOutsideClick(close);
+
+  useEffect(function () {
+    function handleClick(e) {
+      if (ref.current && !ref.current.contains(e.target)) {
+        close();
+      }
+    }
+
+    document.addEventListener("click", handleClick, true);
+
+    return () => document.removeEventListener("click", handleClick, true);
+  }, [close]);
 
   if (name !== openName) return null;
 
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>

3c4b196c752ee9ff323b969160041151761cde90
Adam Rao <adamokoth@protonmail.com>
2024-10-20 23:55:18 +0300

    feat: pagination

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,25 +1,35 @@
 import { getToday } from "../utils/helpers";
+import { RESULTS_PER_PAGE } from "../utils/constants";
+
 import supabase from "./supabase";
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from("bookings")
     .select(
-      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
+      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)",
+      { count: "exact" }
     );
 
   if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
 
   if (sortBy) query = query.order(sortBy.field, { ascending: sortBy.direction === "asc" });
 
-  const { data, error } = await query;
+  if (page) {
+    const from = (page - 1) * RESULTS_PER_PAGE;
+    const to = from + RESULTS_PER_PAGE - 1;
+
+    query = query.range(from, to);
+  }
+
+  const { data, error, count } = await query;
 
   if (error) {
     console.error(error);
     throw new Error("Bookings could not be loaded");
   }
 
-  return data;
+  return { data, count };
 }
 
 export async function getBooking(id) {
